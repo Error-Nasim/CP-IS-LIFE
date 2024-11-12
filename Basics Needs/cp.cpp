a97563e23b883dac24bbfe6b8e6987f2f63dfaeb
Abu Nasim <abu_nasim1033@uits.edu.bd>
2024-11-12 21:58:41 +0600

    Update cp.cpp

--- Basics Needs/cp.cpp
+++ Basics Needs/cp.cpp
@@ -1,1 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int  long long
+#define sp " "
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+#define Yes cout<<"Yes"<<endl
+#define No cout<<"No"<<endl
+#define yes cout<<"yes"<<endl
+#define no cout<<"no"<<endl
 
+ /*
+ YOU CAN FIND THE TRUTH IN MY CODE *_* NASIM
+*/ 
+
+void messi()
+{
+  
+}                                                             
+signed main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(NULL);
+  int tc=1;    cin>>tc;
+  while(tc--)
+  {
+     messi();  
+  }
+    return 0;
+}
