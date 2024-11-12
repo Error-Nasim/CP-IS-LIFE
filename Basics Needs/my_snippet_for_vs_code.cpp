98b843b0ba39b112116493eab93fa3f6df318ff8
Abu Nasim <abu_nasim1033@uits.edu.bd>
2024-11-12 22:02:11 +0600

    Create my_snippet_for_vs_code.cpp

---
+++ Basics Needs/my_snippet_for_vs_code.cpp
@@ -0,0 +1,85 @@
+{
+	"cp": {
+  "prefix": "cpp",
+  "body": [
+    "#include <bits/stdc++.h>",
+    "using namespace std;",
+    "#define int  long long",
+    "#define sp \" \"",
+    "#define YES cout<<\"YES\"<<endl",
+    "#define NO cout<<\"NO\"<<endl",
+    "#define Yes cout<<\"Yes\"<<endl",
+    "#define No cout<<\"No\"<<endl",
+    "#define yes cout<<\"yes\"<<endl",
+    "#define no cout<<\"no\"<<endl",
+    "",
+    " /*",  
+    " YOU CAN FIND THE TRUTH IN MY CODE *_* NASIM",
+   "*/ ",
+   "",
+    "void messi()",
+    "{",
+    "  $0",
+    "}                                                             ",
+    "signed main()",
+    "{",
+    "   ios::sync_with_stdio(false);",
+    "   cin.tie(NULL);",
+    "  int tc=1;   $1 cin>>tc;",
+    "  while(tc--)",
+    "  {",
+    "     messi();  ",
+    "  }",
+    "    return 0;",
+    "}"
+  ],
+  "description": ""
+},
+	  "link_list": {
+		"prefix": "node",
+		"body": [
+		  "class node",
+		  "{",
+		  "public:",
+		  "    int val;",
+		  "    node *next;",
+		  "    node(int val)",
+		  "    {",
+		  "        this->val = val;",
+		  "        this->next = NULL;",
+		  "    }",
+		  "};"
+		],
+		"description": ""
+	  },
+	  "Binary_tree": {
+		"prefix": "bnode",
+		"body": [
+		  "class node ",
+		  "{",
+		  "    public:",
+		  "    int val;",
+		  "    node *right;",
+		  "    node *left;",
+		  "    node(int val)",
+		  "    {",
+		  "        this->val=val;",
+		  "        this->right=NULL;",
+		  "        this->left=NULL;",
+		  "    }",
+		  "};"
+		],
+		"description": ""
+	  },
+
+"vector": {
+  "prefix": "vectorn",
+  "body": [
+    "int n;",
+    "  cin>>n;",
+    "  vector<int>v(n);",
+    "  for(int &x:v) cin>>x;"
+  ],
+  "description": ""
+}
+}
